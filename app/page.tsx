b760eeabf250871d44d9d90c25de65116f723081
SWADHIN300 <swadhinraha81@gmail.com>
2025-12-29 18:42:02 +0530

    intial push

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <p className='text-indigo-300'>
+        Currency
+      </p>
+    </div>
+  )
+}
+
+export default page
